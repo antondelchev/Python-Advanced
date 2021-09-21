06baa1c0f518ac1bfba9b1f894a97665510c8604
antondelchev <dlchv.mail@gmail.com>
2021-09-21 13:57:09 +0300

    Edit.

--- Additions/02. Expression Evaluator.py
+++ Additions/02. Expression Evaluator.py
@@ -1,35 +1,26 @@
 from collections import deque
 
-expression = deque(input().split())
+expression = input().split()
 
-numbers_and_final_result = deque()
+numbers = deque()
 
-while expression:
-    if expression[0].isdigit():
-        numbers_and_final_result.append(int(expression.popleft()))
+math_expressions = {
+    "+": lambda a, b: a + b,
+    "-": lambda a, b: a - b,
+    "*": lambda a, b: a * b,
+    "/": lambda a, b: a // b,
+}
+
+for el in expression:
+    if el in math_expressions:
+        action = math_expressions[el]
+        result = numbers.popleft()
+        while numbers:
+            next_num = numbers.popleft()
+            result = action(result, next_num)
+        numbers.append(result)
+
     else:
-        action = expression.popleft()
-        result = numbers_and_final_result.popleft()
-        if action == "*":
-            while numbers_and_final_result:
-                next_num = numbers_and_final_result.popleft()
-                if (result < 0 and next_num < 0) or (result > 0 and next_num > 0):
-                    result = result * next_num
-                else:
-                    result = - (result * next_num)
+        numbers.append(int(el))
 
-        elif action == "+":
-            while numbers_and_final_result:
-                next_num = numbers_and_final_result.popleft()
-                result = result + next_num
-
-        elif action == "-":
-            while numbers_and_final_result:
-                result = result - numbers_and_final_result.popleft()
-        elif action == "/":
-            while numbers_and_final_result:
-                result = result / numbers_and_final_result.popleft()
-
-        numbers_and_final_result.append(int(result))
-
-print(*numbers_and_final_result)
+print(*numbers)
