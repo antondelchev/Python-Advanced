7967b32e0b22320ca453aac1394e258f799fe86a
antondelchev <dlchv.mail@gmail.com>
2021-10-04 17:52:26 +0300

    Update.

---
+++ File Handling - Exercise/2. Line Numbers.py
@@ -0,0 +1,14 @@
+file = open("text_2.txt", "r")
+
+counter = 1
+for line in file:
+    line = line.strip()
+    punctuation_marks = 0
+    spaces = 0
+    for el in line:
+        if not el.isalpha() and not el == " ":
+            punctuation_marks += 1
+        if el == " ":
+            spaces += 1
+    print(f"Line {counter}: {line} ({len(line) - punctuation_marks - spaces})({punctuation_marks})")
+    counter += 1
